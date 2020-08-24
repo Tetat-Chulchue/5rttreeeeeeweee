5f27cee87ab508125f812f3540fdbab5bb51a6da
laxezice <61070048@kmitl.ac.th>
2020-08-24 19:32:32 +0700

    add method map

--- SOP_Lab1/src/Map.java
+++ SOP_Lab1/src/Map.java
@@ -1,7 +1,22 @@
-
+import java.util.HashMap;
+import java.util.ArrayList;
 public class Map {
+	private ArrayList<HashMap<String, Integer>> monsterPosition = new ArrayList<HashMap<String, Integer>>();
 	private int mapLenght = 10;
 	public Map() {
+		HashMap<String, Integer> mon1  = new HashMap<String, Integer>();
+		mon1.put("x", 7);
+		mon1.put("y", 1);
+		HashMap<String, Integer> mon2  = new HashMap<String, Integer>();
+		mon2.put("x", 2);
+		mon2.put("y", 5);
+		HashMap<String, Integer> mon3  = new HashMap<String, Integer>();
+		mon3.put("x", 7);
+		mon3.put("y", 8);
+		monsterPosition.add(mon1);
+		monsterPosition.add(mon2);
+		monsterPosition.add(mon3);
+		System.out.println(monsterPosition);
 	}
 
 	public void render(int x, int y){
@@ -22,8 +37,14 @@ public class Map {
 		}
 	}
 
-	public boolean cheackPosition(){
-
+	public boolean checkPosition(int x, int y){
+		for(int i=0;i<3;i++){
+			HashMap<String, Integer> monster = this.monsterPosition.get(i);
+			if (x == monster.get("x") && y == monster.get("y")){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
