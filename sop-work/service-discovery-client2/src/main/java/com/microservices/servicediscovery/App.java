b70de1f156b3fce720b8c206528585ef2eedc805
Ogwarf <ome504@hotmail.com>
2020-11-23 18:53:35 +0700

    Update DATABASE

--- sop-work/service-discovery-client2/src/main/java/com/microservices/servicediscovery/App.java
+++ sop-work/service-discovery-client2/src/main/java/com/microservices/servicediscovery/App.java
@@ -3,13 +3,6 @@ package com.microservices.servicediscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.microservices.servicediscovery.bean.Promotion;
 
 @SpringBootApplication
 @EnableDiscoveryClient
---
+++ sop-work/service-discovery-client2/src/main/java/com/microservices/servicediscovery/controller/SQLiteJDBC.java
@@ -0,0 +1,33 @@
+package com.microservices.servicediscovery.controller;
+import java.sql.*;
+
+public class SQLiteJDBC {
+	public static void main( String args[] ) {
+	      Connection c = null;
+	      Statement stmt = null;
+	      
+	      try {
+	         Class.forName("org.sqlite.JDBC");
+	         c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	         System.out.println("Opened database successfully");
+
+	         stmt = c.createStatement();
+	         String sql = "CREATE TABLE PROMOTION " +
+	                        "(id INT PRIMARY KEY     NOT NULL," +
+	                        " name           TEXT    NOT NULL, " + 
+	                        " method         TEXT     NOT NULL, " + 
+	                        " description    TEXT, " + 
+	                        " expDate        DATE, " +
+	                        " amount		 INT 	NOT NULL, " +
+	                        " min		 	 INT 	NOT NULL, " +
+	                        " max		 	 INT 	NOT NULL) "; 
+	         stmt.executeUpdate(sql);
+	         stmt.close();
+	         c.close();
+	      } catch ( Exception e ) {
+	         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	         System.exit(0);
+	      }
+	      System.out.println("Table created successfully");
+	   }
+	}
